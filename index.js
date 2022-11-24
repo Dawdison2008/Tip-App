7c9ca45e6489fd9c8ec823c3a9bceb88a72d62ed
Dawid Błaszczak <dawidson2008@o2.pl>
2022-11-24 19:34:38 +0100

    first commit add all files

---
+++ index.js
@@ -0,0 +1,17 @@
+// const btnPer5 = document.querySelector('.per5')
+// const btnPer10 = document.querySelector('.per10')
+// const btnPer15 = document.querySelector('.per15')
+// const btnPer25 = document.querySelector('.per25')
+// const btnPer50 = document.querySelector('.per50')
+
+// btnPer5.addEventListener('click', () =>{
+//   btnPer5.classList.add('active')
+// })
+
+
+
+
+
+
+
+
